519fe643509186f5e8f8e29eaea3b1db6b40cb08
lakshman <lakshmankarthik1986@gmail.com>
2020-07-11 02:58:36 +0530

    initial project setup

---
+++ src/main/java/com/ibm/Javaapp/App.java
@@ -0,0 +1,21 @@
+package com.ibm.Javaapp;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+
+public class App 
+{
+	public static void main(String[] args) {
+		
+		int first =10;
+		int last =20;
+		
+		int sum = first+last;
+       
+        System.out.println("the sum is :" +sum);
+    }
+}
